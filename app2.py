bccfc64aae7314a122ed4bb55f5128725a0dc100
Andreia Alpe <andreiaalpe@gmail.com>
2026-09-23 10:55:59 -0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,12 +1,29 @@
+# cria uma função
+def biscoito():
+    x = "está com fome!"
+    print("Joca", x)
+
+
 #name = "Joca da Silva"
 #age=43
+
+
 name = input("Qual o seu nome?")
 age = input("Quantos anos você tem?")
-print()
 
-print()
+age = int(age)
+
+print(type (name))
+print(type(age))
+older = age + 10 
+print(f"{name} terá {older} daqui a 10")
+
 print("Olá", name, "!")
 print()
 print("Você tem", age, "anos")
+
 print()
+
+biscoito()
+
 
