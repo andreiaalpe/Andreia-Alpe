be40855504ffa99d6c41c5c5013a70756efde6b6
Andreia Alpe <andreiaalpe@gmail.com>
2026-09-23 09:29:01 -0300

    Ok

---
+++ app1.py
@@ -0,0 +1,20 @@
+#Este aplicativo serve pra alguma coisa
+
+""" Isso é um bloco de comentários.
+Posso ter várias linhas.
+O Python ign0ora tudo isso. 
+Independentemente da quantidade de linhas. """
+'''Isso também funciona'''
+
+
+
+
+
+
+# Cria as variáveis para receber os dados do usuário
+
+name = "Joca da Silva"
+frase = 'EStou com "fome"'
+frase = "Estou com \"fome\""
+# Não pode então, "Estou com \"fome\"". è melhor usar aspas simples.
+print (frase) 
---
+++ app2.py
@@ -0,0 +1,13 @@
+#name = "Joca da Silva"
+#age=43
+name = input("Qual o seu nome?")
+age = input("Quantos anos você tem?")
+print()
+
+print()
+print("Olá", name, "!")
+print()
+print("Você tem", age, "anos")
+print()
+
+
